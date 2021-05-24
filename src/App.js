7b8fec3ecebf0260751d74664370a6e78b41214f
KellyW-coder <79504971+KellyW-coder@users.noreply.github.com>
2021-05-24 17:35:42 +0200

    add GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather Search app</div>;
+  return (
+    <div className="App">
+      <h1>Weather Search app</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="Https://www.kccd.co.za" target="_blank">
+          {" "}
+          Kelly Williams
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/KellyW-coder/react-weatherapp-2021"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
